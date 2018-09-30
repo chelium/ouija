a914e3251b88bd64862926d28cd62cad651a7c85
Chris He <chris7he@gmail.com>
2018-09-30 15:57:04 -0500

    Display basic web page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 import tornado.ioloop
 import tornado.web
+import tornado.locks
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -8,13 +10,33 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("Hello, world")
 
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+class Application(tornado.web.Application):
+
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+        ]
+        settings = dict(
+            template_path = os.path.join(os.path.dirname(__file__), "templates"),
+            static_path = os.path.join(os.path.dirname(__file__), "static"),
+            debug = True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+
+class HomeHandler(tornado.web.RequestHandler):
+
+    async def get(self):
+        self.render("home.html")
+
+
+async def main():
+    app = Application()
+    app.listen("8888")
+
+    shutdown_event = tornado.locks.Event()
+    await shutdown_event.wait()
 
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.current().run_sync(main)
